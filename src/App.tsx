5da7f91a9f3980c39358ab0b9ac527158b1224c2
Petr Dudy <dudypetr1@seznam.cz>
2026-08-14 23:22:18 +0200

    Add route for AdminVenuePage in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 import { AdminLoginPage } from './pages/admin/AdminLoginPage'
 import { AdminDashboardPage } from './pages/admin/AdminDashboardPage'
+import { AdminVenuePage } from './pages/admin/AdminVenuePage'
 import { ProtectedRoute } from './components/ProtectedRoute'
 import { TablePage } from './pages/public/TablePage'
 
@@ -12,6 +13,7 @@ export function App() {
       <Route path="/admin/login" element={<AdminLoginPage />} />
       <Route element={<ProtectedRoute />}>
         <Route path="/admin" element={<AdminDashboardPage />} />
+        <Route path="/admin/hospoda/:venueId" element={<AdminVenuePage />} />
       </Route>
 
       <Route path="/v/:venueSlug/t/:tableToken" element={<TablePage />} />
